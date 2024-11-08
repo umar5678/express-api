45db77b42043693b0c1f5e1b0bd032bbabfc5455
Umar Farooq <umarfarooq567800@gmail.com>
2024-11-08 16:54:27 +0500

    contollers done

--- task_manager/controlers/tasks_controllers.js
+++ task_manager/controlers/tasks_controllers.js
@@ -1,32 +1,77 @@
-import { Task } from "../models/task_model.js"
+import { Task } from "../models/task_model.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
-const getAllTasks = (req, res) => {
-res.send("All taks are here ")
-}
+const getAllTasks =  async (req, res) => {
+  try {
+    const allTasks = await Task.find({});
+    res.status(200).json({tasks: allTasks});
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+};
 
 const createTask = async (req, res) => {
-   try {
-    const task = await Task.create(req.body)
-     res.status(201).json(task)
-   } catch (error) {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const getSingleTask = async (req, res) => {
+  // const id = req.params.id
+  try {
+    const task = await Task.findOne({ _id: req.params.id })
+    
+    if (!task) {
+        return res.status(404).json({msg: `No task find with id ${req.params.id}`})
+      }
+    
+    res.status(200).json({ task: task })
+    
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
+};
+
+const UpdateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {new: true, runValidators: true})
+
+    if (!task) {
+      return res.sttus(404).json({ msg: `Task with id: ${taskID} not found` })
+      
+    }
+
+    res.status(200).json(task)
+    
+  } catch (error) {
+      res.status(500).json(error);
+  }
+
+};
+
+const deleteSingleTask = async (req, res) => {
+  
+  const {id: taskID} = req.params
+  try {
+    const deletedTask = await Task.findOneAndDelete({ _id: taskID })
+
+    if (!deletedTask) {
+      return res.status(404).json({msg: `No task with this id: ${taskID} found`})
+    }
+
+    res.status(200).json(deletedTask)
+  } catch (error) {
     res.status(500).json(error)
-Z    }
-}
+  }
 
-const getSingleTask = (req, res) => {
-  res.send(`get single task of id: ${req.params.id}`);
 };
 
-const UpdateTask = (req, res) => {
-  res.send(`update single task of id: ${req.params.id}`);
-};
-
-const deleteSingleTask = (req, res) => {
-  res.send(`delete single task of id: ${req.params.id}`);
-};
-
-export {getAllTasks, createTask, getSingleTask,UpdateTask , deleteSingleTask}
-
-
+export { getAllTasks, createTask, getSingleTask, UpdateTask, deleteSingleTask };
 
 // starting again
